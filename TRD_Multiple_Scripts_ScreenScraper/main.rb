8b745862941d34686cdb76e6cced2c21eeb4a1c1
Bulmaro Herrera <bulmaro.herrera@fujifilm.com>
2023-02-24 16:32:56 -0800

    Refactoring for elegance

--- TRD_Multiple_Scripts_ScreenScraper/main.rb
+++ TRD_Multiple_Scripts_ScreenScraper/main.rb
@@ -4,33 +4,52 @@ require 'date'
 
 # Get the script list from a file
 def read_script_list(file_name)
+  script_list_field_names = [:owner, :job, :script_name]
+
   scripts = []
   File.open(file_name, "r") do |file|
     file.each_line do |line|
       next if line.strip.empty?
-      fields = line.split(" ")
-      scripts << { :owner => fields[0], :job => fields[1], :script_name => fields[2] }
+      # Add a hash of pairs, where for each pair, the key is the field name, and the value is field value
+      values = line.split(" ")
+      script = {}
+      script_list_field_names.each_index do |index|
+        script[script_list_field_names[index]] = values[index]
+      end
+      scripts << script
     end
   end
+
   scripts
 end
 
-# Extract a field by name
+# Extract the value (from inner_html) from a cell by field_name
 def get_field_value(result_info, field_name)
-  field = result_info.at_css("[id=#{field_name}]")
-  # Remove leading & trailing spaces, tabs & new lines, multiple contiguous spaces and HTML tags
-  field.inner_html.strip.gsub(/[\n\t]/, '').gsub(/\s+/, ' ').gsub(/<\/?[^>]*>/, '')
+  field = result_info.at_css("[id=#{field_name}]").inner_html
+  # Remove tabs & new lines, multiple contiguous spaces and HTML tags
+  field.gsub(/[\n\t]/, '').gsub(/\s+/, ' ').gsub(/<\/?[^>]*>/, '').strip
+end
+
+# Extract the link from a cell by field_name
+def get_field_link(result_info, field_name)
+  "#{result_info.at_css("[id=#{field_name}]").inner_html.match(/href="([^"]+)"/)[1]}"
 end
 
 # Read all the results for all the 'scripts'
 def get_results(scripts, days_ago)
   # Set the maximum age for a result
   today = Time.now.to_date
-  starting_from_date = today - days_ago
-  puts "Retrieving results from last #{days_ago} days (from #{starting_from_date} to #{today})"
+  starting_from = today - days_ago
+  puts "Retrieving results from last #{days_ago} days (from #{starting_from} to #{today})"
 
   # TRD root
   trd_root_url = "http://phx-test-results.sonosite.com"
+
+  # Fields we want to extract from table
+  field_names_on_page = %w[product bom build start_time result failure_reasons name]
+  field_names_on_file = %w[prod_type bom version time_stamp result reasons link]
+  default_values = %w[_ _ _ _ _ n/a _]
+  extra_field_names_on_file = %w[analyzed_on jira_bug_link]
 
   # Visit the 'results page' for all scripts
   script_results = []
@@ -38,37 +57,36 @@ def get_results(scripts, days_ago)
     print "Retrieving results for '#{script[:script_name]}' (from Jenkins job '#{script[:job]}')"
 
     # build the URL for the TRD query
-    trd_results_url = "#{trd_root_url}/script_results?script_path=#{script[:script_name]}"
+    trd_results_url = "#{trd_root_url}/script_results?script_path=#{script[:script_name]}&start_date=#{starting_from}"
     # navigate to the page and get the HTML content
     page_content = Nokogiri::HTML(open(trd_results_url))
     # extract the table
     results_table = page_content.css('#script_results').first
 
-    # Initialize the 'script result' hash with same fields as the 'script'
-    script_result = script.clone
-
     # Select recent results from the table
     results = []
     results_table.xpath('.//tr').each do |result_row|
       result_info = result_row.css('td')
-      time_stamp = DateTime.parse(get_field_value(result_info, "start_time"))
-      if time_stamp.to_date >= starting_from_date
-        # Extract certain fields from the current 'result'
-        result = {}
-        result["product"] = get_field_value(result_info, "product")
-        result["bom"] = get_field_value(result_info, "bom")
-        result["sw_ver"] = get_field_value(result_info, "build")
-        result["time_stamp"] = time_stamp.to_s
-        result["result"] = get_field_value(result_info, "result")
-        failure_reason = get_field_value(result_info, "failure_reasons")
-        result["reason"] = (failure_reason.empty? ? "n/a" : failure_reason)
-        result["detail_link"] = "#{trd_root_url}#{result_info.at_css("[id=#{'name'}]").inner_html.match(/href="([^"]+)"/)[1]}"
-        results << result
+      # Extract selected fields from the current 'result_info'
+      result = {}
+      field_names_on_page.length.times do |i|
+        field_name_on_page = field_names_on_page[i]
+        if field_name_on_page == "name"
+          value = trd_root_url + get_field_link(result_info, field_name_on_page)
+        else
+          value = get_field_value(result_info, field_name_on_page)
+        end
+        result[field_names_on_file[i]] = value.empty? ? default_values[i] : value
       end
+      extra_field_names_on_file.each do |extra_field_name_on_file|
+        result[extra_field_name_on_file] = "TBD"
+      end
+      results << result
     end
 
     puts " - #{results.size} results"
 
+    script_result = script.clone
     script_result["results"] = results
     script_results << script_result
   end
@@ -95,13 +113,13 @@ def generate_csv_file(file_name, script_results)
     if script_results.length > 0
       # Generate header using first result's keys
       header = script_results.first
-      file.write("#{(header.keys[0...-1] | header['results'].first.keys).join(',')}\n")
+      file.write("#{(header.keys[0...-1] + header['results'].first.keys).join(',')}\n")
 
       # Write each result
       script_results.each do |script_result|
         results = script_result["results"]
         results.each do |result|
-          file.write("#{(script_result.values[0...-1] | result.values).join(",")}\n")
+          file.write("#{(script_result.values[0...-1] + result.values).join(",")}\n")
         end
         total_results += results.size
       end
@@ -115,13 +133,13 @@ end
 script_list = read_script_list("script_list.txt")
 
 # Read the results from TRD
-days_ago = (ARGV.size == 0 ? 30 : ARGV[0].to_i)
+days_ago = (ARGV[0] || "30").to_i
 results_all = get_results(script_list, days_ago)
 
 # Filter some results
 results_always_passing = select_with_result(results_all, "pass")
 results_always_failing = select_with_result(results_all, "fail") # could be "fail|aborted|unrun"
-results_inconsistent = results_all - results_always_passing - results_always_fail_failing
+results_inconsistent = results_all - results_always_passing - results_always_failing
 
 # Generate result files
 results_file_location = "results"
