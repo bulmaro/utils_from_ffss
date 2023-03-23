7bdd929233a11df026066173b8222cd5bb108709
Bulmaro Herrera <bulmaro.herrera.contractor@fujifilm.com>
2023-03-22 17:38:00 -0700

    Enhanced KeepAlive to kill the process upon exit

--- KeepSysAppProcessAlive.ps1
+++ KeepSysAppProcessAlive.ps1
@@ -1,4 +1,4 @@
-function Sleep-A-Bit([string]$Activity, [string]$Status, [int]$SleepTime) {
+function Wait-A-Bit([string]$Activity, [string]$Status, [int]$SleepTime) {
     if ($SleepTime -eq 0) # When no SleepTime was specified, briefly show info with no progress bar
     {
         Write-Progress -Activity "$(Get-TimeStamp): ${Activity}" -Status $Status
@@ -20,22 +20,36 @@ $ProcessName = "SysApp"
 $ExecutablePath = "C:\FFSS\debug\x64\Host\SysApp\SysApp.exe"
 $MonitoringMessage = "Keeping '$ProcessName' alive."
 
+$sig = '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+Add-Type -MemberDefinition $sig -name NativeMethods -namespace Win32
+
 Write-Host "------------------------------------------"
 Write-Host "Script to keep process '$ProcessName' alive by running '$ExecutablePath'."
 Write-Host "Press Ctrl-C to stop."
 Write-Host "------------------------------------------"
 
-while ($true) {
-    Sleep-A-Bit $MonitoringMessage "Checking if process is running..."
-    $process = Get-Process -Name $ProcessName -ErrorAction SilentlyContinue
+try
+{
+    while ($true)
+    {
+        Wait-A-Bit $MonitoringMessage "Checking if process is running..."
+        $process = Get-Process -Name $ProcessName -ErrorAction SilentlyContinue
 
-    if ($process -eq $null) {
-        Write-Host -NoNewLine "$(Get-TimeStamp): Process '$ProcessName' is not running, starting it..."
-        Start-Process $ExecutablePath
-        Sleep-A-Bit "Rebooting '$ProcessName'." "Waiting to stabilize, hopefully in 30 sec..." 30
-        Write-Host " Started."
-        Continue
+        if ($null -eq $process)
+        {
+            Write-Host -NoNewLine "$( Get-TimeStamp ): Process '$ProcessName' is not running, starting it..."
+            Start-Process $ExecutablePath
+            Wait-A-Bit "Rebooting '$ProcessName'." "Waiting to stabilize, hopefully in 10 sec..." 10
+            Write-Host " Started."
+            $handle = @(Get-Process $ProcessName)[0].MainWindowHandle
+            $null = [Win32.NativeMethods]::ShowWindowAsync($handle, 3)
+            Continue
+        }
+
+        Wait-A-Bit $MonitoringMessage "Sleeping 20 sec..." 20
     }
-
-    Sleep-A-Bit $MonitoringMessage "Sleeping 20 sec..." 20
+}
+finally {
+    Write-Host "$( Get-TimeStamp ): Now killing '$ProcessName'."
+    Stop-Process -Name $ProcessName -Force
 }
